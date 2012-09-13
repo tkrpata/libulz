3943179885b4e6a25dce89363e3b9dcd234ec4c0
Mark Ellzey <mark.thomas@mandiant.com>
2012-09-13 10:56:36 -0400

    Added recursive in your functionality.

--- ulz.c
+++ ulz.c
@@ -34,3 +34,21 @@ ulz_has_anyone_really_been_far_even_as_decided_to_use_even_go_want_to_do_look_mo
     }
 }
 
+int
+ulz_yo_dawg_i_hurd_u_like_ulz_so_i_put_a_ulz_in_ur_ulz(struct ulz * in_ur) {
+    struct ulz * ulz;
+
+    if (!in_ur) {
+        return -0x90;
+    }
+
+    if ((ulz = ulz_new(in_ur)) == NULL) {
+        return -666;
+    }
+
+    ulz->next   = in_ur;
+    in_ur->next = ulz;
+
+    return 0;
+}
+
